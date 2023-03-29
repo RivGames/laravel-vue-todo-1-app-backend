25403d0f2dfb4a162e5bf6e26dde4803e796a957
Ihor Rud <Igorryd7@gmail.com>
2023-03-29 18:17:47 +0300

    Виконав два сідер.

--- database/migrations/2023_03_29_151344_create_todos_table.php
+++ database/migrations/2023_03_29_151344_create_todos_table.php
@@ -11,12 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('todos', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('title');
+            $table->string('body',255);
+            $table->boolean('done')->default(false);
             $table->timestamps();
         });
     }
@@ -26,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('todos');
     }
 };
