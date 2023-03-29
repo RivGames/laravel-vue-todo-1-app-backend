f98099c99ddede734721ff7274998b5d794f610a
Ihor Rud <Igorryd7@gmail.com>
2023-03-29 20:04:15 +0300

    Поставив префікс

--- routes/groups/account.php
+++ routes/groups/account.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\Api\v1\Account\AccountController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(AccountController::class)->group(function(){
-    Route::post('register','register')->name('account.register');
+
+Route::prefix('account/v1')->group(function(){
+    Route::controller(AccountController::class)->group(function(){
+        Route::post('register','register')->name('account.register');
+    });
 });
